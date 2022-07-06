c527cca8bada183767e6360c2254952002f76c1f
William Leung <gwilliamleung@gmail.com>
2022-07-06 19:02:16 -0400

    WIP

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let display = document.querySelector('.result')
+let display = document.getElementById('display');
+
 let buttons = Array.from(document.getElementsByClassName('button'));
 
 buttons.map( button => {
@@ -6,6 +7,18 @@ buttons.map( button => {
         switch(e.target.innerText){
             case 'C':
                 display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
                 break;
             default:
                 display.innerText += e.target.innerText;
